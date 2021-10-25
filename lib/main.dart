eb30cc1ab25a64be6acd13c447dd020b78ec57c5
pau.ballber <pau.ballber@est.fib.upc.edu>
2021-10-25 19:15:57 +0200

    HomePage base and bottomNavigationBar views

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_application_1/views/home.dart';
+import 'package:flutter_application_1/app.dart';
 
 void main() {
   // Make statusBar background transparent
@@ -10,17 +10,4 @@ void main() {
     ),
   );
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.grey),
-      darkTheme: ThemeData.dark(),
-      home: HomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
 }
